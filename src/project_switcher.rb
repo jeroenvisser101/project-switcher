c30eeaeed5ed0a279bf73c60ef5f7d342f78a738
Jeroen Visser <jeroenvisser101@gmail.com>
2015-02-02 10:45:10 +0100

    Fixed the updating system.
     - added exit statements on non-switch commands.
     - added `-v` option to check application's version.
     - added release note link after update.

--- src/project_switcher.rb
+++ src/project_switcher.rb
@@ -13,9 +13,12 @@ class ProjectSwitcher
       end
 
       opts.on('--self-update', 'Update project-switcher to it\'s latest version') do
-        echo `cd ~/.project-switcher && git pull`
-        echo ''
-        echo 'Project is up-to-date.'
+        update!
+        exit
+      end
+
+      opts.on('-v', '--version', 'Prints the application\'s version') do
+        echo "v#{ version }"
         exit
       end
 
@@ -26,11 +29,13 @@ class ProjectSwitcher
 
       opts.on('-e', '--edit', 'Opens the configuration file in the default editor') do
         exec '"${EDITOR:-vi}" ~/.projects.yml'
+        exit
       end
 
       opts.on('-r', '--reload', 'Reloads the configuration file') do
         settings true
         echo 'Project config reloaded'
+        exit
       end
 
       opts.on('--uninstall', 'Removes project-switcher from your system') do
@@ -40,15 +45,15 @@ class ProjectSwitcher
         echo '  eval "$(project-switcher --inject)"'
         echo ''
         echo 'And remove ~/.projects.yml yourself.'
+        exit
       end
 
       opts.on('-h', '--help', 'Prints this help.') do
         help!
       end
-
-      opts.on
     end
 
+    # Parse existing options
     @parser.parse!
 
     project = ARGV.first
@@ -61,6 +66,32 @@ class ProjectSwitcher
       switch_to! project
     end
 
+  end
+
+  # Updates the package
+  def update!
+    old_tag = version
+    `cd ~/.project-switcher && git pull`
+    new_tag = version
+
+    # Check if the tag was updated.
+    if old_tag != new_tag
+      echo "Updated to v#{ new_tag }"
+      print_release_notes! new_tag
+    else
+      echo "You have the latest version (v#{ new_tag }) already!"
+    end
+  end
+
+  # Returns the current version
+  def version
+    `cd ~/.project-switcher && git describe --tags`.chomp
+  end
+
+  # Prints the url to the release notes of the latest version
+  def print_release_notes!(tag)
+    echo 'If you want to know what has been changed, check out our changelog,'
+    echo "http://github.com/jeroenvisser101/project-switcher/releases/tag/#{tag}"
   end
 
   # Switches directories to specified project
@@ -81,6 +112,7 @@ class ProjectSwitcher
     end
   end
 
+  # Runs hooks specified in ~/.projects.yml
   def run_hook(type)
     if type == :before
       exec config['before_switch'] unless config['before_switch'].nil?
@@ -94,6 +126,7 @@ class ProjectSwitcher
     exec "#{ config['alias'] } () { eval \"$(project-switcher $@)\" }"
   end
 
+  # Prints help for the application
   def help!
     echo @parser.help
     echo ''
@@ -102,32 +135,38 @@ class ProjectSwitcher
     exit
   end
 
-  def settings(force = false)
-    if force
+  # (re)loads all configuration in the memory.
+  def settings(reload = false)
+    if reload
       @settings = YAML.load_file File.expand_path('~/.projects.yml')
     else
       @settings ||= YAML.load_file File.expand_path('~/.projects.yml')
     end
   end
 
+  # Returns all projects configured in ~/.projects.yml
   def projects
     settings['projects']
   end
 
+  # Returns program config
   def config
     settings['config']
   end
 
+  # Prints all available projects
   def print_projects!
     projects.each do |key, project|
       echo "  #{ key }: #{ project['name'] } (#{ project['path'] })"
     end
   end
 
+  # Echo's a string
   def echo(str)
     puts "echo '#{ str.gsub(/'/, "'\"'\"'") }'"
   end
 
+  # This executes it in the user's shell
   def exec(arg)
     puts arg
   end
